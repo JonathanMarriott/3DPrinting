02984c403ac85a14c9e0b1bceb213123133188a8
Jonathan Marriott <34217286+jmarriott78@users.noreply.github.com>
2020-12-03 18:42:52 +0000

    Added function to unzip a given sl1 file to a tmp folder (Needs validation)

--- src/main/java/FileConversion/FileInput.java
+++ src/main/java/FileConversion/FileInput.java
@@ -1,4 +1,4 @@
-package main.FileConversion;
+package FileConversion;
 
 import java.io.ByteArrayOutputStream;
 import java.awt.image.BufferedImage;
@@ -6,12 +6,16 @@ import java.io.File;
 import javax.imageio.ImageIO;
 import java.util.Arrays;
 import java.util.Scanner;
+import org.zeroturnaround.zip.ZipUtil;
 
 public class FileInput {
   public static void main(String[] args) throws Exception {
     Scanner inputScanner = new Scanner(System.in);
     System.out.println("Enter input filename: ");
     String fileName = inputScanner.nextLine();
+    System.out.println("Enter input sl1 filename: ");
+    String sl1file = inputScanner.nextLine();
+    Sl1opener(sl1file);
     inputScanner.close();
     BufferedImage bImage = ImageIO.read(new File(fileName));
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -21,4 +25,9 @@ public class FileInput {
     System.out.println(Arrays.toString(data));
   }
 
+  public static void Sl1opener(String fileName){
+    File output = new File("."+File.separator+"tmp"+File.separator+"pngs");
+    ZipUtil.unpack(new File(fileName), output);
+
+  }
 }
