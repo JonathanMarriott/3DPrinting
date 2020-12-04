083058c30db12887afe472808490d446afddbc62
Jonathan Marriott <34217286+jmarriott78@users.noreply.github.com>
2020-12-04 17:57:24 +0000

    Added function to convert a Png to an array of bitSets

--- src/main/java/FileConversion/FileInput.java
+++ src/main/java/FileConversion/FileInput.java
@@ -1,12 +1,16 @@
 package FileConversion;
 
-import java.io.ByteArrayOutputStream;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
-import java.util.Arrays;
-import java.util.Scanner;
+import ar.com.hjg.pngj.ImageLineInt;
+import ar.com.hjg.pngj.PngReader;
 import org.zeroturnaround.zip.ZipUtil;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Scanner;
 
 public class FileInput {
   public static void main(String[] args) throws Exception {
@@ -21,13 +25,45 @@ public class FileInput {
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
     ImageIO.write(bImage, "png", bos);
     byte[] data = bos.toByteArray();
-
-    System.out.println(Arrays.toString(data));
+//    System.out.println(Arrays.toString(data));
+//    pngToBitSets("./tmp/pngs/Original_Prusa_SL1_Calibration_test_object_v200000.png");
   }
 
-  public static void Sl1opener(String fileName){
+
+/*
+  Takes an Sl1 filename (assumed to be validated eg not a random zip of unknown files)
+   extracts the contents(including config.ini) to the directory /tmp/pngs
+  */
+public static void Sl1opener(String fileName){
     File output = new File("."+File.separator+"tmp"+File.separator+"pngs");
     ZipUtil.unpack(new File(fileName), output);
 
+  }
+
+  /*
+  Take png filename as input, returns an array of bitSets, each bitSet is a row of the image
+  The values are set so that if a pixel is black the corresponding bit is set to false, otherwise it is set to true
+  */
+  public static BitSet[] pngToBitSets(String pngFile){
+    PngReader reader = new PngReader(new File(pngFile));
+    int rows = reader.getImgInfo().rows;
+    int cols = reader.getImgInfo().cols;
+    BitSet[] outSet = new BitSet[rows];
+    System.out.println(reader.toString());
+    int i = 0;
+    while(reader.hasMoreRows()) {
+      outSet[i] = new BitSet(cols);
+      int[] currentLine = ((ImageLineInt) reader.readRow()).getScanline();
+      for(int j=0;j<cols;j++){
+        if(currentLine[j]==0){
+          outSet[i].set(j,false);
+        }
+        else{
+          outSet[i].set(j,true);
+        }
+      }
+      i++;
+    }
+    return outSet;
   }
 }
