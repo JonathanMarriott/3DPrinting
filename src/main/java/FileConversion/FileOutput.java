82c6d21b8cdb5ca56ffc43e5220848ab74c02d81
Jonathan Marriott <34217286+jmarriott78@users.noreply.github.com>
2020-12-15 15:25:41 +0000

    Added method to convert model and output as an sl1 file

--- src/main/java/FileConversion/FileOutput.java
+++ src/main/java/FileConversion/FileOutput.java
@@ -1,12 +1,17 @@
-package spe_pack;
+package FileConversion;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.BitSet;
+import java.util.Properties;
 
-import ar.com.hjg.pngj.ImageInfo;
-import ar.com.hjg.pngj.ImageLineHelper;
-import ar.com.hjg.pngj.ImageLineInt;
-import ar.com.hjg.pngj.PngWriter;
+import ar.com.hjg.pngj.*;
+import org.zeroturnaround.zip.ZipUtil;
+import org.zeroturnaround.zip.commons.FileUtils;
+
+import javax.imageio.ImageIO;
 
 /**
  * @author saadelmoutaouakil
@@ -30,7 +35,7 @@ public class FileOutput {
         ImageLineInt image_line = new ImageLineInt(info);
 
         for (int r = 0; r < info.rows; r++) {
-            for (int i = 0; i < info.cols; i++) {
+            for (int i = 0; i < info.cols-1; i++) {
                 if (input[r].get(i)) {
                     ImageLineHelper.setPixelRGB8(image_line, i, WHITE_RGB, WHITE_RGB, WHITE_RGB);
                 } else {
@@ -42,6 +47,56 @@ public class FileOutput {
         }
         
         writer.end();
+    }
+    public static void export (BitSet[] data,File outputFile,ImageInfo info) {
+        int height = info.rows;
+        int width = info.cols;
+        BufferedImage outImage = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (data[i].get(j)) {
+                    outImage.setRGB(j, i, 0xffffffff);
+                } else {
+                    outImage.setRGB(j, i, 0x00000000);
+                }
+            }
+        }
+        try {
+            // Save as PNG
+            ImageIO.write(outImage, "png", outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static File modelToPngs(BitSet[][] model, File configFile,File pngExample){
+        Properties config = new Properties();
+        try (FileInputStream in = new FileInputStream(configFile)) {
+            config.load(in);
+        }catch(Exception e){
+            System.out.println("Error: No config file found");
+        }
+        String jobDir = (String) config.get("jobDir");
+        PngReader reader = new PngReader(pngExample);
+        File outDir = new File("."+File.separator+"tmp"+File.separator+"out");
+        outDir.mkdir();//ok to ignore
+        for(int i = 0; i< model.length; i++){
+            File outFile = new File("."+File.separator+"tmp"+File.separator+"out"+File.separator+jobDir+String.format("%05d",i)+".png");
+            try {
+                outFile.createNewFile();//ok to ignore
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            export(model[i],outFile,reader.getImgInfo());
+        }
+        try {
+            FileUtils.copyFile(configFile,new File("."+File.separator+"tmp"+File.separator+"out"+File.separator+"config.ini"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new File("."+File.separator+"tmp"+File.separator+"out");
+
+
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.Objects;
 import java.util.Scanner;
 
 import static FileConversion.FileInput.Sl1opener;
 import static FileConversion.FileInput.pngToBitSets;
 
+import FileConversion.FileOutput;
 import IslandDetection.IslandDetection;
+import org.zeroturnaround.zip.ZipUtil;
 
 public class Main {
     public static void main(String[] args){
@@ -22,10 +25,11 @@ public class Main {
             sl1file = inputScanner.nextLine();
         }
         while (!sl1file.endsWith(".sl1")) { // Check File ends with .sl1
-            System.out.println("Enter a filename ending in sl1: ");
+            System.out.println("Enter a input filename ending in sl1: ");
             sl1file = inputScanner.nextLine();
         }
         inputScanner.close();
+        System.out.println("File opening");
         File pngDir = Sl1opener(sl1file); // Extracts the file and returns the directory
         File[] pngFiles = pngDir.listFiles(pathname -> pathname.getName().endsWith(".png")); //Filters for png files
         assert pngFiles != null;
@@ -34,11 +38,32 @@ public class Main {
             output.add(pngToBitSets(png));
         }
         BitSet[][] result = output.toArray(new BitSet[output.size()][]);
-        
+
+        System.out.println("Checking for Islands");
         byte[][][] stateModel = IslandDetection.checkIslands(result);
-
+        System.out.println("Adding Supports");
         BitSet[][] supportedModel = Supporter.buildSupportsBasic(stateModel);
+        //BitSet[][] supportedModel = result;
+        System.out.println("Creating new File");
+        File supportedDir = FileOutput.modelToPngs(supportedModel,
+                Objects.requireNonNull(pngDir.listFiles(path -> path.getName().equals("config.ini")))[0],
+                pngFiles[0]);
+        File outFile = new File(sl1file.substring(0,sl1file.length()-4)+"SUPPORTED.sl1");
+        ZipUtil.pack(supportedDir,outFile);
+        deleteDirectory(supportedDir);
+        System.out.println("Supported file at: "+outFile.getName());
 
 
+
+    }
+
+    private static boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
     }
 }
